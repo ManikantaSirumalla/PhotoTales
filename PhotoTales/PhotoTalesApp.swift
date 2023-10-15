cac8a52caf630422a94e491b3163418d8cbb6f31
Manikanta Sirumalla <Manikanta.sirumalla@icloud.com>
2023-10-15 22:52:02 +0530

    Initial Commit

---
+++ PhotoTales/PhotoTalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoTalesApp.swift
+//  PhotoTales
+//
+//  Created by Manikanta Sirumalla on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoTalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
